21300e8cdb2eaaa2f192fb6c52ea4738194ca0b6
hammoh7 <soahammohaadkar@gmail.com>
2024-06-03 11:14:16 +0530

    Styling

---
+++ frontend/src/theme.js
@@ -0,0 +1,105 @@
+export const colorTokens = {
+    grey: {
+      0: "#FFFFFF",
+      10: "#F6F6F6",
+      50: "#f8fafc",
+      100: "#f1f5f9",
+      200: "#e2e8f0",
+      300: "#cbd5e1",
+      400: "#94a3b8",
+      500: "#64748b",
+      600: "#475569",
+      700: "#334155",
+      800: "#1e293b",
+      900: "#0f172a",
+      1000: "#000000",
+    },
+    primary: {
+      50: "#f0fdfa",
+      100: "#ccfbf1",
+      200: "#99f6e4",
+      300: "#5eead4",
+      400: "#2dd4bf",
+      500: "#14b8a6",
+      600: "#0d9488",
+      700: "#0f766e",
+      800: "#115e59",
+      900: "#134e4a",
+    },
+  };
+  
+  // mui theme settings
+  export const themeSettings = (mode) => {
+    return {
+      palette: {
+        mode: mode,
+        ...(mode === "dark"
+          ? {
+              // palette values for dark mode
+              primary: {
+                dark: colorTokens.primary[200],
+                main: colorTokens.primary[500],
+                light: colorTokens.primary[800],
+              },
+              neutral: {
+                dark: colorTokens.grey[100],
+                main: colorTokens.grey[200],
+                mediumMain: colorTokens.grey[300],
+                medium: colorTokens.grey[400],
+                light: colorTokens.grey[700],
+              },
+              background: {
+                default: colorTokens.grey[900],
+                alt: colorTokens.grey[800],
+              },
+            }
+          : {
+              // palette values for light mode
+              primary: {
+                dark: colorTokens.primary[700],
+                main: colorTokens.primary[500],
+                light: colorTokens.primary[50],
+              },
+              neutral: {
+                dark: colorTokens.grey[700],
+                main: colorTokens.grey[500],
+                mediumMain: colorTokens.grey[400],
+                medium: colorTokens.grey[300],
+                light: colorTokens.grey[50],
+              },
+              background: {
+                default: colorTokens.grey[10],
+                alt: colorTokens.grey[0],
+              },
+            }),
+      },
+      typography: {
+        fontFamily: ["Rubik", "sans-serif"].join(","),
+        fontSize: 12,
+        h1: {
+          fontFamily: ["Rubik", "sans-serif"].join(","),
+          fontSize: 40,
+        },
+        h2: {
+          fontFamily: ["Rubik", "sans-serif"].join(","),
+          fontSize: 32,
+        },
+        h3: {
+          fontFamily: ["Rubik", "sans-serif"].join(","),
+          fontSize: 24,
+        },
+        h4: {
+          fontFamily: ["Rubik", "sans-serif"].join(","),
+          fontSize: 20,
+        },
+        h5: {
+          fontFamily: ["Rubik", "sans-serif"].join(","),
+          fontSize: 16,
+        },
+        h6: {
+          fontFamily: ["Rubik", "sans-serif"].join(","),
+          fontSize: 14,
+        },
+      },
+    };
+  };
